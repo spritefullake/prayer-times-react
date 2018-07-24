09c4ee1f02364df21f2e31cda83b8957293c9822
spritefullake <42ahmad@gmail.com>
2018-07-24 00:53:55 -0400

    timezone data w WebSQL

--- src/common/utils.js
+++ src/common/utils.js
@@ -2,6 +2,9 @@
 import getData from './data'
 import { DateTime, Interval } from 'luxon'
 
+import { Location, SQLite, FileSystem, Asset } from 'expo'
+
+
 export function getSizing(data, ctx, decPlaces = 100) {
 
     const tops = data.map(i => i.time).map(invl => {
@@ -50,4 +53,84 @@ export function nextPrayer({ date, coords }) {
         nextPrayerName: names[times.indexOf(nextPrayerTime)],
         nextPrayerEnd: nextPrayerTime
     }
+}
+
+
+export async function findAddress(address, coords) {
+    //if the address was already 
+    //reverse geocoded, there
+    //is no use in using an 
+    //expensive operation again
+    if (address) {
+        return Promise.reject("address already exists")
+    }
+
+    let res = await Location.reverseGeocodeAsync({ latitude: coords[0], longitude: coords[1] });
+    return res;
+}
+
+export function findTimeZone(coords){
+    
+}
+
+export async function downloadTimeZoneData() {
+
+    const dbName = 'newbie.db';
+
+    //if the db has already been populated,
+    //skip having to execute expensive sql statements
+    if ((await FileSystem.getInfoAsync(`${FileSystem.documentDirectory}/SQLite/${dbName}`)).exists) {
+        return;
+    }
+
+    //use Asset expo lib to get uri
+    const localURI = Asset.fromModule(require(`./db/times.sql`)).uri;
+    const targetURI = `${FileSystem.documentDirectory}/SQLite/timmy.db`;
+
+    //download the db file to the device
+
+    await FileSystem.downloadAsync(
+        localURI,
+        targetURI
+    );
+
+    const dbString = await FileSystem
+        .readAsStringAsync(
+            targetURI
+        );
+
+
+
+    //split the sql file into statements, making sure 
+    //to include the semicolons that delimit statements
+    const dbStatements = dbString.split(";")
+        .filter(statement => statement != "")
+        .map(statement => statement + ";");
+
+
+    const db = SQLite.openDatabase(dbName)
+
+    db.transaction(async tx => {
+
+        for (let statement of dbStatements) {
+            doStatement(tx, statement);
+        }
+
+
+    },
+        function txError(err) {
+            console.log("An error occured with the transaction ", err);
+        },
+        function txSuccess() {
+            console.log("The transaction was successful!!!")
+        }
+    );
+}
+
+
+function doStatement(tx, statement, arr = [],
+    onSuccess = (suc, res) => console.log("The result of the tx is ", res),
+    onError = (err) => console.log("there was a db error, ", err)) {
+
+    tx.executeSql(statement, arr, onSuccess, onError);
 }
